3da79268976c1d65bccc510c2ec1d56e59c9267b
dineth-md123 <dinethsenura400@gmail.com>
2025-02-12 10:12:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || '',
+SESSION_ID: process.env.SESSION_ID || 'h390WZYD#IBQKspXe1nOvydz4Y1DxoXefoO-eXnKOxQnFfeGScJ0',
 ALIVE_IMG: process.env.ALIVE_IMG || "url",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm Senu Md whatsapp bot ♥️ I'm Alive Now ♥️",
 };
